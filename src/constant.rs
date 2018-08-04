8ea4a200b8e9f4d3e4ad804f61290bda6e09b1ea
amejii <nanahachiroku786@gmail.com>
2018-08-04 20:17:59 +0900

    make constant numbers definition public

--- src/constant.rs
+++ src/constant.rs
@@ -1,3 +1,3 @@
-const K_PI : f64 = 3.14159265358979323846;
-const K_INF : f64 = 1e128;
-const K_EPS : f64 = 1e-6;
+pub const K_PI : f64 = 3.14159265358979323846;
+pub const K_INF : f64 = 1e128;
+pub const K_EPS : f64 = 1e-6;
