c37f99dd2aa9157ca52ab5446d1c80ef248ec642
amejii <nanahachiroku786@gmail.com>
2018-08-09 08:52:47 +0900

    add ppm file save function

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod constant;
 pub mod intersection;
 pub mod random;
 pub mod material;
+pub mod ppm;
---
+++ src/ppm.rs
@@ -0,0 +1,47 @@
+use material::Color;
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use std::path::Path;
+
+fn clamp(x:f64) -> f64{
+    if x < 0.0{
+        return 0.0;
+    }        
+    if x > 1.0{
+        return 1.0;
+    }
+
+    x
+}
+
+fn to_int (x: f64) -> i32{
+    //because rust does not allow to calcutaion between i32 and f64,
+    //make all constant value as floating point.
+    (clamp(x).powf(1.0/2.2) * 255.0 + 0.5) as i32
+}
+
+pub fn save_ppm_file(filename : &str, image : &[Color], width : i32, height : i32){
+    let path = format!("{}{}","out/".to_string(),filename);
+    let path = Path::new(&path);
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", path.display(), why.description()),
+        Ok(file) => file,
+    };
+    
+    let header = format!("P3\n{} {}\n{}\n",width.to_string(),height.to_string(),255.to_string());
+    match file.write_all(header.as_bytes()){
+        Err(why) => {
+            panic!("couldn't write to {}: {}", path.display(),
+                                               why.description())
+        },
+        Ok(_) => println!("successfully wrote to {}", path.display()),
+    }
+    
+    for element in image.iter(){
+        let data = format!("{} {} {} ",to_int(element.x),to_int(element.y),to_int(element.z));
+        println!("{}", data);
+        file.write_all(data.as_bytes());
+    }
+}
